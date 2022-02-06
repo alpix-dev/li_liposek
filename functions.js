4032f0eabb2e47481368fd5938642ad4924b3a44
eduvlemes notebook <edu.vlemes@gmail.com>
2022-02-06 14:59:22 -0300

    hi

---
+++ functions.js
@@ -0,0 +1,58 @@
+const ls = [];
+
+ls.influencers = [
+    {
+        img : 'https://via.placeholder.com/386x368',
+        title: 'Letícia Gabriela',
+        subtitle : 'Influenciadora',
+        text : '@ig.leticiagabriela'
+    },
+    {
+        img : 'https://via.placeholder.com/386x368',
+        title: 'Bruna Rocha',
+        subtitle : 'Influenciadora',
+        text : '@brunadcr'
+    },
+    {
+        img : 'https://via.placeholder.com/386x368',
+        title: 'Letícia Calatroia',
+        subtitle : 'Influenciadora',
+        text : '@lecatroia'
+    },
+    {
+        img : 'https://via.placeholder.com/386x368',
+        title: 'Rafaella Tassila',
+        subtitle : 'Influenciadora',
+        text : '@rafaellatassila'
+    },
+    {
+        img : 'https://via.placeholder.com/386x368',
+        title: 'Giovanna Fazann',
+        subtitle : 'Influenciadora',
+        text : '@giovanna_fazann'
+    }
+]
+
+ls.testimonials = [
+    {
+        title: 'Elis Guimarães',
+        subtitle : 'Emagreci 20kg usando o LipoSek',
+        date : '02 de fevereiro de 2022',
+        description : 'Eu comprei meio desconfiado, mas depois de 3 meses usando, consegui mudar a         minha vida. Emagreci 20 kg e reconquistei a minha autoestima. Recomendo! ',
+        stars : 4
+    },
+    {
+        title: 'Gabriela Sanches de Oliveira',
+        subtitle : 'Eu amei! ',
+        date : '01 de fevereiro de 2022',
+        description : 'Cumpre o que promote! Adorei a sensação de mais energia durante o dia',
+        stars : 4
+    },
+    {
+        title: 'Maria Luiza Bittencourt',
+        subtitle : 'Melhor produto de emagrecimento!',
+        date : '01 de fevereiro de 2022',
+        description : 'Um dos melhores produtos do mercado. Minha amiga me indicou e agora eu indico pra todo mundo. Pode confiar de olho fechado',
+        stars : 4
+    }
+]
